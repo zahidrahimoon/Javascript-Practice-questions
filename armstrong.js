b37b9034a46bc4a73ed8d27e01be153437ac240c
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-01-13 23:25:34 -0800

    Armstrong.js

--- armstrong.js
+++ armstrong.js
@@ -1,2 +1,15 @@
 let number = prompt("Enter the number");
-let temp = 0;
+let temp = number;
+let sum = 0;
+
+while (temp > 0) {
+  let digit = temp % 10;
+  sum += digit ** 3;
+  temp = parseInt(temp / 10);
+}
+
+if (sum == number) {
+  console.log(`${number} is an Armstrong Number`);
+} else {
+  console.log(`${number} is not an Armstrong Number`);
+}
