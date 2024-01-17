93b91b1f3acd0509859ead839489eb2c6e8743db
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-01-17 09:08:50 -0800

    Add files via upload

---
+++ odd-evenfunction.js
@@ -0,0 +1,7 @@
+let number =13; //any number
+
+function oddEven(number){
+     return number % 2 === 0 ? 'Even' :'Odd';  //condition
+}
+let result =oddEven(number);
+console.log(`${number} is ${result}`);  //result 
