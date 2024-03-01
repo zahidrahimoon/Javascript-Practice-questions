50a395c86f0346f4862d4e7413462e91a52589ea
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-03-01 05:53:33 -0800

    Add files via upload

---
+++ BasicProblems/last&firststringchange.js
@@ -0,0 +1,8 @@
+let string = "hello";
+
+let firstCharacter = 'h';
+let lastCharacter = "o";
+
+let newString = lastCharacter + string + firstCharacter;
+
+console.log(newString);
---
+++ BasicProblems/stringtofirst&end.js
@@ -0,0 +1,7 @@
+let string = "hello";
+
+let firstCharacter = 'h';
+
+let newString =  firstCharacter + string + firstCharacter;
+
+console.log(newString);
